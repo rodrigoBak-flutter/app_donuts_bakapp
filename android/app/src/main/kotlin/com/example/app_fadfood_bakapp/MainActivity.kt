10fc31adfb9ee5943b3679e9857412d1ff16aa98
Rodrigo <rodrigosebastianbak@hotmail.com>
2023-04-25 11:40:46 +0100

    app_donuts_bakApp

---
+++ android/app/src/main/kotlin/com/example/app_fadfood_bakapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_fadfood_bakapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/home/homepage.dart
@@ -0,0 +1,134 @@
+import 'package:app_fadfood_bakapp/data/dummy_data.dart';
+import 'package:app_fadfood_bakapp/ui/home/home_widgets.dart';
+import 'package:flutter/material.dart';
+import 'tabs/donut/donut_tab/donut_tab.dart';
+import 'tabs/smoothie/smoothie_tab/smoothie_tab.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController =
+        TabController(length: tabBarItems.length, vsync: this);
+    _tabController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: tabBarItems.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.grey[800],
+              size: 36,
+            ),
+            onPressed: () {},
+          ),
+          actions: [
+            IconButton(
+              padding: const EdgeInsets.only(right: 10),
+              icon: Icon(
+                Icons.person_rounded,
+                color: Colors.grey[800],
+                size: 36,
+              ),
+              onPressed: () {
+                // account button tapped
+              },
+            ),
+          ],
+        ),
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: Column(
+                children: [
+                  const IWantToEat(),
+                  const SizedBox(
+                    height: 15,
+                  ),
+
+                  // tab bar
+                  TabBar(
+                      controller: _tabController,
+                      isScrollable: true,
+                      indicatorWeight: 0.01,
+                      padding: EdgeInsets.zero,
+                      indicatorPadding: EdgeInsets.zero,
+                      labelPadding: EdgeInsets.zero,
+                      tabs: [
+                        for (int i = 0;
+                            i < tabBarItems.length;
+                            i++) ...[
+                          TabIcon(
+                              item: tabBarItems[i],
+                              isSelected:
+                                  _tabController.index == i)
+                        ]
+                      ]),
+
+                  // tab bar view
+                  Expanded(
+                    child: TabBarView(
+                      controller: _tabController,
+                      children: const [
+                        // donut page
+                        // SingleChildScrollView(
+                        //   child: Column(
+                        //     children: [
+
+                        //     ],
+                        //   ),
+                        // ),
+
+                        DonutTab(),
+
+                        // burger page
+                        Center(
+                          child: Text("Burger"),
+                        ),
+
+                        // smoothie page
+                        SmoothieTab(),
+                        // pancake page
+                        Center(
+                          child: Text("Pancake"),
+                        ),
+                        // pizza page
+                        Center(
+                          child: Text("Pizza"),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Positioned.fill(
+                bottom: 5,
+                top: MediaQuery.of(context).size.height * 0.78,
+                child: const ViewCart())
+          ],
+        ),
+      ),
+    );
+  }
+}
